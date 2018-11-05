ed1919826987e5c3444a02e7b43df12de8772ad9
nobuo <nobuosato@gmail.com>
2018-11-05 11:19:49 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'N. Sato'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'V0'
+release = u'0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -77,8 +77,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
